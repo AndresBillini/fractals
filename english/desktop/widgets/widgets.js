d2959774dfc20aaff241a6fa84437d72d3793b93
DESKTOP-79RM8UR\andre <andres_g_1000@hotmail.com>
2017-08-31 14:38:02 -0400

    First series of changes, tags, css, widgets (using JSON)

--- english/desktop/widgets/widgets.js
+++ english/desktop/widgets/widgets.js
@@ -1,4 +1,4 @@
-function widgets(x){
+function merwebo(x){
     var test = x;
         
     if (window.XMLHttpRequest) {
@@ -19,3 +19,33 @@ function widgets(x){
           xmlhttp.open('GET','widgets/wdg.php?name='+test,true);
           xmlhttp.send();
 }
+
+function widgets(x) {
+    $.getJSON("../../JSON/wdg.json", function (data) {
+        var items;
+        console.log();
+        items = data.home;
+        switch (x) {
+            case 1:
+                items += data.secondary.services.menu;
+                items += data.secondary.services.contact;
+                break;
+            case 2: 
+                items += data.secondary.portfolio.menu;
+                items += data.secondary.portfolio.contact;
+                break;
+            case 3:
+                items += data.secondary.about.menu;
+                items += data.secondary.about.contact;
+                break;
+            case 4:
+                items += data.secondary.experiments.menu;
+                items += data.secondary.experiments.contact;
+                break;
+        }
+        document.getElementById("fs-contact").innerHTML = data.contact;
+        document.getElementById("fs-menu").innerHTML = items;
+        document.getElementById("fs-footer").innerHTML = data.footer;
+    });
+}
+    
